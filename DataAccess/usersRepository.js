550d833f67fc382dc0f81f74f1be9c00d789d628
Yair Revivo <yrevivo@attentigroup.com>
2020-04-17 20:01:16 +0300

    add mongo

---
+++ DataAccess/usersRepository.js
@@ -0,0 +1,37 @@
+var mongoose = require('mongoose');
+
+class usersRepository {
+    constructor() {
+        mongoose.connect('mongodb://127.0.0.1:27017/Itunes', { useNewUrlParser: true, useUnifiedTopology: true });
+        this.db = mongoose.connection;
+        this.db.on('error', console.error.bind(console, 'connection error:'));
+        this.db.once('open', function () {
+            console.log.bind(console, 'DB connected')
+        });
+        this.UserSchema = new mongoose.Schema({
+            userName: String,
+        });
+    }
+
+    async inserUser(userName) {
+        var User = mongoose.model('user', this.UserSchema);
+        var newUser = new User({ userName: userName });
+        User.findOne({ 'userName': userName }, function (err, fetchedUser) {
+            if (err) return handleError(err);
+            console.log(fetchedUser)
+            if (fetchedUser == undefined) {
+                newUser.save(function (err, user) {
+                    if (err) return console.error(err);
+                    console.debug(user.username)
+                });
+            }
+        });
+    }
+}
+
+module.exports = new usersRepository();
+
+
+
+
+
